ddc7fd564a6906c4138c5179b858da26470a5a3d
whyweplay30 <1559396326@qq.com>
2024-04-14 19:00:13 +0800

    [M]提交代码

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
+const getNavList = function (filePath) {
+  const list = [];
+  // 同步读取目录
+  const files = fs.readdirSync(filePath);
+  files.forEach((file) => {
+    const curFilePath = path.join(filePath, file);
+    const stats = fs.statSync(curFilePath);
+    let item = null;
+    if (stats.isFile()) {
+      item = {
+        text: file.split(".")[0],
+        link: filePath.replace("./docs", "") + "/"+file,
+      };
+    } else {
+      item = {
+        text: file,
+        items: getNavList(filePath + "/" + file),
+      };
+    }
+    console.log(JSON.stringify(item));
+    list.push(item);
+  });
+  return list;
+};
+console.log(getNavList("./docs/pages"));
+const navList = [
+  
+  ...getNavList("./docs/pages"),
+];
+module.exports = {
+  title: "whyweplay",
+  base: "/home",
+  description: "Just playing around.",
+  themeConfig: {
+    nav: navList,
+    sidebar: {
+      // "/pages/CSS": [
+      //   {
+      //     text: "CSS文章目录",
+      //     items: [
+      //       { text: "盒模型", link: "/pages/CSS/pages/盒模型.md" },
+      //       // { text: "JavaScript", link: "/pages/JavaScript" },
+      //     ],
+      //   },
+      //   { text: "CSS", item: [] },
+      // ],
+    },
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,30 @@
+---
+layout: home
+hero:
+  name: why we play
+  text: 个人博客
+  tagline: Somebody has to win, so why not me?
+  actions:
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/whyweplay30
+features:
+  - icon: ⚡️
+    title:  vue3
+    details: 前端框架
+    link: "https://cn.vuejs.org/"
+  - icon: 🖖
+    title: MDN Web Docs
+    details: 前端技术文档
+    link: "https://developer.mozilla.org/zh-CN/"
+  - icon: 🛠️
+    title: vitePress
+    details: 快速开发个人博客的工具
+    link: "https://vitepress.dev/zh/"
+---
+<style>
+:root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
+}
+</style>
